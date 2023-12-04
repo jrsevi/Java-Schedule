4d643c77712a61df5d1ee5e9fa91dd33db5ec1af
jrsevi <jrsevi@gmail.com>
2023-12-04 13:47:02 -0800

    Final Commit

--- Assets/script.js
+++ Assets/script.js
@@ -2,55 +2,28 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-
-//* Current Date *//
-function currentDay(){
-var today= dayjs();
-$("#currentDay").text(today.format("dddd, MMMM D, YYYY"));
-}
-currentDay();
-
-//* This adds the past, present, or future class to the time slots on the schedule*//
-function classId(){
-    $(".time-block").each(function(){
-        var time = dayjs().hour();
-        var blockTime = parseInt($(this).attr("id").split("-")[1]);
-        console.log(blockTime);
-        if (blockTime < time){
-            $(this).addClass("past");
-        } else if (blockTime == time){
-            $(this).addClass("present");
-        } else {
-            $(this).addClass("future");
-        }
-    });
-}
-classId();
-
-
-
-
-
-
-
 $(function () {
 
     //* Current Date *//
-function currentDay(){
-    var today= dayjs();
-    $("#currentDay").text(today.format("dddd, MMMM D, YYYY"));
+    function currentDay() {
+        var today = dayjs();
+        $("#currentDay").text(today.format("dddd, MMMM D, YYYY"));
     }
     currentDay();
-    
+
     //* This adds the past, present, or future class to the time slots on the schedule*//
-    function classId(){
-        $(".time-block").each(function(){
+    function classId() {
+        $(".time-block").each(function () {
             var time = dayjs().hour();
             var blockTime = parseInt($(this).attr("id").split("-")[1]);
+            //*This prints the saved entry to the time slot*//
+            var id = $(this).attr("id");
+            var savedEntry = localStorage.getItem(id);
+            $(this).children(".description").val(savedEntry);
             console.log(blockTime);
-            if (blockTime < time){
+            if (blockTime < time) {
                 $(this).addClass("past");
-            } else if (blockTime == time){
+            } else if (blockTime == time) {
                 $(this).addClass("present");
             } else {
                 $(this).addClass("future");
@@ -60,8 +33,8 @@ function currentDay(){
     classId();
 
     //* This saves the user input to local storage*//
- function saveInput(){
-        $(".saveBtn").on("click", function(){
+    function saveInput() {
+        $(".saveBtn").on("click", function () {
             var time = $(this).parent().attr("id");
             var input = $(this).siblings(".description").val();
             localStorage.setItem(time, input);
@@ -69,25 +42,23 @@ function currentDay(){
     }
     saveInput();
 
-    //* This gets the user input from local storage*//
+})
+// TODO: Add a listener for click events on the save button. This code should
+// use the id in the containing time-block as a key to save the user input in
+// local storage. HINT: What does `this` reference in the click listener
+// function? How can DOM traversal be used to get the "hour-x" id of the
+// time-block containing the button that was clicked? How might the id be
+// useful when saving the description in local storage?
+//
+// TODO: Add code to apply the past, present, or future class to each time
+// block by comparing the id to the current hour. HINTS: How can the id
+// attribute of each time-block be used to conditionally add or remove the
+// past, present, and future classes? How can Day.js be used to get the
+// current hour in 24-hour time?
+//
+// TODO: Add code to get any user input that was saved in localStorage and set
+// the values of the corresponding textarea elements. HINT: How can the id
+// attribute of each time-block be used to do this?
+//
+// TODO: Add code to display the current date in the header of the page.
 
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
-  
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Java-Schedule
-A schedule application made using Html, CSS, and Java Script
+
+## Description
+
+This is a schedule application that was made using HTML, CSS, Java Script, and JQeury.
+I was provided starter code for this application, but the functionality of the application was created by me.
+## Installation
+
+n/a
+
+## Usage
+To use the Schedule Application, navigate to the time slot you wish to create an event. Click inside the middle row, write your event title, and then click the save button on the right of the corresponding row. After this, the event will be saved to your local storage and will be displayed on the schedule application until local storage is cleared.
+
+To use this page visit the link: https://jrsevi.github.io/Java-Schedule/index.html
+
+![Webpage Screenshot](./Assets/Screenshot%202023-12-04%20134602.png)
+
+## Credits
+
+Created by Jeremy Sevilla
+
+## License
+
+MIT License
+
+Copyright (c) 2023 jrsevi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+---
